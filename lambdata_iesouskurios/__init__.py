c6b9d2338bb583efd83800aae1dcc9fc9322fb0b
iesous-kurios <j.wylie.81@gmail.com>
2020-01-21 21:17:37 -0800

    some functions

--- lambdata_iesouskurios/__init__.py
+++ lambdata_iesouskurios/__init__.py
@@ -5,6 +5,21 @@ lambdata_iesouskurios
 A collection of Data Science Helper functions
 
 """
+import numpy as np 
+import pandas as pd 
 
-for i in range(1, 10):
-    print(i)
+print('You have successfully imported "lambdata_iesouskurios"')
+
+print('Please type "lambdata_iesouskurios.HELPME" without the quotation marks for a list of functions')
+ 
+  
+# initialize list of lists 
+data = [['func1', 'use this to ____ your dataframe'], 
+       ['func2', 'use this to ____ your dataframe'], 
+       ['func3', 'this can ___ your dataframe']] 
+  
+# Create the pandas DataFrame 
+HELPME = pd.DataFrame(data, columns = ['Utility', 'Function']) 
+  
+# print dataframe. 
+ 
--- lambdata_iesouskurios/df_utils.py
+++ lambdata_iesouskurios/df_utils.py
@@ -5,4 +5,8 @@ Utility functions for working with dataframes
 
 import pandas as pd
 
-TEST_DF = pd.DataFrame([1,2,3])
+# change the path to df to where your datafile is stored
+
+df = pd.read_excel('~/unit3/repos/iesous-kurios/lambdata/90Day.xlsx')
+
+NO_NULL_DF = df.isna()
